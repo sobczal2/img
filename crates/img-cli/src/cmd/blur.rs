1d9ac3c0bf80857dfdcddf814c8491b40f085af1
sobczal2 <81999946+sobczal2@users.noreply.github.com>
2025-06-16 22:42:19 +0200

    implement gaussian blur

--- crates/img-cli/src/cmd/blur.rs
+++ crates/img-cli/src/cmd/blur.rs
@@ -1,13 +1,22 @@
 use std::path::PathBuf;
 
-use clap::{arg, value_parser, ArgMatches, Command};
-use img::ops::blur::mean::mean_blur;
+use clap::{arg, value_parser, ArgMatches, Command, ValueEnum};
+use img::{
+    image::Image,
+    ops::blur::{gaussian::gaussian_blur, mean::mean_blur},
+};
 
 use crate::io::{read_image, write_image};
 
 use super::common::{input_arg, output_arg, INPUT_ARG_NAME, OUTPUT_ARG_NAME};
 
 pub const CMD_NAME: &str = "blur";
+
+#[derive(ValueEnum, Clone)]
+pub enum BlurAlgorithm {
+    Mean,
+    Gaussian,
+}
 
 pub fn subcommand() -> Command {
     Command::new(CMD_NAME)
@@ -18,12 +27,35 @@ pub fn subcommand() -> Command {
                 .required(true)
                 .value_parser(value_parser!(usize)),
         )
+        .arg(
+            arg!(-a --algorithm <algorithm> "blur algorith")
+                .default_value("gaussian")
+                .value_parser(value_parser!(BlurAlgorithm)),
+        )
+        .arg(
+            arg!(-s --sigma <sigma> "sigma value")
+                .default_value("3")
+                .value_parser(value_parser!(f32)),
+        )
 }
 
 pub fn action(matches: &ArgMatches) -> anyhow::Result<()> {
     let image = read_image(matches.get_one::<PathBuf>(INPUT_ARG_NAME).unwrap())?;
-    let target_radius = matches.get_one::<usize>("radius").unwrap();
-    let image = mean_blur(&image, *target_radius)?;
+    let image = match matches.get_one::<BlurAlgorithm>("algorithm").unwrap() {
+        BlurAlgorithm::Mean => apply_mean(&image, matches)?,
+        BlurAlgorithm::Gaussian => apply_gaussian(&image, matches)?,
+    };
     write_image(&image, matches.get_one::<PathBuf>(OUTPUT_ARG_NAME).unwrap())?;
     Ok(())
+}
+
+fn apply_mean(image: &Image, matches: &ArgMatches) -> anyhow::Result<Image> {
+    let target_radius = matches.get_one::<usize>("radius").unwrap();
+    Ok(mean_blur(image, *target_radius)?)
+}
+
+fn apply_gaussian(image: &Image, matches: &ArgMatches) -> anyhow::Result<Image> {
+    let target_radius = matches.get_one::<usize>("radius").unwrap();
+    let sigma = matches.get_one::<f32>("sigma").unwrap();
+    Ok(gaussian_blur(image, *target_radius, *sigma)?)
 }
--- crates/img/src/ops/blur/gaussian.rs
+++ crates/img/src/ops/blur/gaussian.rs
@@ -4,7 +4,7 @@ use thiserror::Error;
 
 use crate::{
     error::{IndexResult, OutOfBoundsError},
-    image::{idx_to_xy, Image},
+    image::Image,
     math::{idx_to_xy, xy_to_idx},
 };
 
@@ -13,6 +13,8 @@ use crate::{
 pub enum Error {
     #[error("radius too big for given image")]
     RadiusTooBig,
+    #[error("invalid sigma - has to be positive")]
+    InvalidSigma,
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
@@ -20,12 +22,15 @@ pub type Result<T> = std::result::Result<T, Error>;
 /// perform mean blur on an image not in place
 /// this reduces size of an image by radius * 2 times
 /// so to receive image of an original size you should pad it
-pub fn mean_blur(image: &Image, radius: usize) -> Result<Image> {
-    validate(image, radius)?;
+pub fn gaussian_blur(image: &Image, radius: usize, sigma: f32) -> Result<Image> {
+    validate(image, radius, sigma)?;
 
+    let kernel = &GaussianKernel::new(radius, sigma);
     let diamater = radius * 2 + 1;
     let mut new_image =
         Image::empty((image.size().0 - diamater + 1, image.size().1 - diamater + 1));
+
+    let radius_i32 = radius as i32;
 
     new_image.rows_mut().for_each(|(y, row)| {
         row.for_each(|(x, mut px)| {
@@ -33,11 +38,17 @@ pub fn mean_blur(image: &Image, radius: usize) -> Result<Image> {
                 .flat_map(|k_y| {
                     (0..diamater).map(move |k_x| {
                         let new_px = unsafe { image.pixel_unchecked((x + k_x, y + k_y)) };
-                        (new_px.r(), new_px.g(), new_px.b())
+                        let offset = (k_x as i32 - radius_i32, k_y as i32 - radius_i32);
+                        let kernel_value = unsafe { kernel.value_unchecked(offset) };
+                        (
+                            new_px.r() as f32 * kernel_value,
+                            new_px.g() as f32 * kernel_value,
+                            new_px.b() as f32 * kernel_value,
+                        )
                     })
                 })
                 .fold((0f32, 0f32, 0f32), |(acc_r, acc_g, acc_b), (r, g, b)| {
-                    (acc_r + r as u32, acc_g + g as u32, acc_b + b as u32)
+                    (acc_r + r, acc_g + g, acc_b + b)
                 });
 
             px.set_r(sum.0 as u8);
@@ -50,14 +61,19 @@ pub fn mean_blur(image: &Image, radius: usize) -> Result<Image> {
     Ok(new_image)
 }
 
-fn validate(image: &Image, radius: usize) -> Result<()> {
+fn validate(image: &Image, radius: usize, sigma: f32) -> Result<()> {
     if image.size().0 < radius * 2 + 1 || image.size().1 < radius * 2 + 1 {
         return Err(Error::RadiusTooBig);
+    }
+
+    if sigma <= 0f32 {
+        return Err(Error::InvalidSigma);
     }
 
     Ok(())
 }
 
+#[derive(Debug)]
 pub struct GaussianKernel {
     values: Box<[f32]>,
     radius: usize,
@@ -67,13 +83,17 @@ impl GaussianKernel {
     pub fn new(radius: usize, sigma: f32) -> GaussianKernel {
         let diameter = radius * 2 + 1;
         let mut values = vec![0f32; diameter * diameter].into_boxed_slice();
-        let divisor_inverse = 1f32 / (diameter * diameter) as f32;
 
         values.iter_mut().enumerate().for_each(|(idx, value)| {
-            let (x, y) = idx_to_xy(idx, radius);
+            let (x, y) = idx_to_xy(idx, diameter);
             let offset = (x as i32 - radius as i32, y as i32 - radius as i32);
-            *value = gaussian_fn(offset, sigma) * divisor_inverse;
+            *value = gaussian_fn(offset, sigma);
         });
+
+        let sum = values.iter().sum::<f32>();
+        let sum_inv = 1f32 / sum;
+
+        values.iter_mut().for_each(|value| *value *= sum_inv);
 
         GaussianKernel { values, radius }
     }
@@ -112,8 +132,14 @@ fn offset_to_xy(offset: (i32, i32), radius: usize) -> IndexResult<(usize, usize)
 }
 
 fn gaussian_fn(offset: (i32, i32), sigma: f32) -> f32 {
-    let (offset_x, offset_y) = offset;
-    let base = (1f32 / (2f32 * PI * sigma * sigma)) * E;
-    let exponent = -((offset_x * offset_x + offset_y * offset_y) as f32 / (2f32 * sigma * sigma));
-    base.powf(exponent)
+    let sigma_2 = sigma * sigma;
+    let x_2 = (offset.0 * offset.0) as f32;
+    let y_2 = (offset.1 * offset.1) as f32;
+
+    (1f32 / (2f32 * PI * sigma_2)) * E.powf(-(x_2 + y_2) / (2f32 * sigma_2))
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
 }

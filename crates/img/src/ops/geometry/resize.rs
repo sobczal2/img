55e091bd0e4475def2bd68d9aa926b0b69b4f736
Łukasz Sobczak <81999946+sobczal2@users.noreply.github.com>
2025-08-28 18:28:05 +0200

    fix resize scaling

--- crates/img/src/ops/geometry/resize.rs
+++ crates/img/src/ops/geometry/resize.rs
@@ -20,10 +20,12 @@ pub fn resize(image: &Image, scale: Scale) -> Result<Image> {
     let new_size = scale.apply(image.size())?;
     let mut new_image = Image::empty(new_size);
 
+    let inverse_scale = scale.inverse();
+
     new_image.rows_mut().for_each(|(y, row)| {
         row.for_each(|(x, mut px)| {
             // SAFETY: nearest function should always return a valid point in original image
-            px.copy_from_pixel(unsafe { image.pixel_unchecked(scale.translate(Point::new(x, y))) });
+            px.copy_from_pixel(unsafe { image.pixel_unchecked(inverse_scale.translate(Point::new(x, y))) });
         });
     });
 
@@ -35,10 +37,12 @@ pub fn resize_par(image: &Image, scale: Scale) -> Result<Image> {
     let new_size = scale.apply(image.size())?;
     let mut new_image = Image::empty(new_size);
 
+    let inverse_scale = scale.inverse();
+
     new_image.rows_mut().par_bridge().for_each(|(y, row)| {
         row.for_each(|(x, mut px)| {
             // SAFETY: nearest function should always return a valid point in original image
-            px.copy_from_pixel(unsafe { image.pixel_unchecked(scale.translate(Point::new(x, y))) });
+            px.copy_from_pixel(unsafe { image.pixel_unchecked(inverse_scale.translate(Point::new(x, y))) });
         });
     });
 

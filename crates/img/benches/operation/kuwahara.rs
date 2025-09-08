2d42bdd588f8c65bd78b9d2ee828743bb4b7eab8
Łukasz Sobczak <81999946+sobczal2@users.noreply.github.com>
2025-09-08 09:49:33 +0200

    benchmark kuwahara filter

---
+++ crates/img/benches/operation/kuwahara.rs
@@ -0,0 +1,19 @@
+use criterion::{
+    Criterion,
+    black_box,
+    criterion_group,
+    criterion_main,
+};
+use img::{
+    operation::blur::kuwahara::{
+        kuwahara,
+        kuwahara_par,
+    },
+};
+
+mod common;
+
+operation_bench!(kuwahara[]);
+
+criterion_group!(benches, kuwahara_benchmark);
+criterion_main!(benches);

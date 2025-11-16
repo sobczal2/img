e1cc8fadf4eedf9e90818019ced3fcdf5f659fdc
Łukasz Sobczak <81999946+sobczal2@users.noreply.github.com>
2025-11-16 13:21:30 +0100

    fix comment

--- crates/img/src/component/primitive/limits.rs
+++ crates/img/src/component/primitive/limits.rs
@@ -1,5 +1,5 @@
 /// Maximum dimension size of an image (width or height).
-/// Guaranted to be less than isize::MAX.
+/// Guaranteed to be less than isize::MAX.
 #[cfg(target_pointer_width = "64")]
 pub const DIMENSION_MAX: usize = (1u64 << 32) as usize - 1;
 
